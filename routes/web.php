5b0eb661387975b8ab424f9de249368192ca81e6
andrebagus <andreandrearshav@gmail.com>
2026-01-11 16:18:58 +0700

    pembaruan

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
+// Route::get('/run-migrate', function () {
+//     Artisan::call('migrate', ["--force" => true]);
+//     return "Migration berhasil dijalankan!";
+// });
 // Route::post('/register', [AuthController::class, 'register']);
 // Route::post('/login', [AuthController::class, 'login']);
